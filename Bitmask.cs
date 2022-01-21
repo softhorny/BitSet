ebb8703f0c0faf01e208a81ca8ecf1e9b80023fe
bipodev <88240298+bipodev@users.noreply.github.com>
2022-01-21 21:24:46 +0300

    Create Bitmask.cs

---
+++ Bitmask.cs
@@ -0,0 +1,28 @@
+public class Bitmask
+{
+    public Bitmask(int length)
+    {
+        if (length < 0)
+            throw new ArgumentOutOfRangeException();
+
+        _length = length;
+
+        _bits = new byte[((length - 1) >> 3) + 1];
+    }
+
+    private byte[] _bits;
+    private int _length;
+
+    public int Length => _length;
+
+    private bool Get(int key) => (_bits[key >> 3] & (1 << key)) != 0;
+    private void Set(int key, bool value) 
+    {
+        if(value) 
+            _bits[key >> 3] |= (byte)(1 << key);
+        else 
+            _bits[key >> 3] &= (byte)~(1 << key);
+    }
+
+    public bool this[int key] { get => Get(key); set => Set(key, value); }
+}
