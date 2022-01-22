4c1b9f5dcee4e59a7f26612d7626e7b12371d29a
bipodev <88240298+bipodev@users.noreply.github.com>
2022-01-22 23:01:26 +0300

    Update Bitset.cs

--- Bitset.cs
+++ Bitset.cs
@@ -22,6 +22,8 @@ public class Bitset
         for(int i = 0; i < length; i++)
             _bits[i] = 0;
     }
+    
+    public bool this[int key] { get => Get(key); set => Set(key, value); }
 
     private bool Get(int key) => (_bits[key >> 3] & (1 << key)) != 0;
     
@@ -32,6 +34,4 @@ public class Bitset
         else 
             _bits[key >> 3] &= (byte)~(1 << key);
     }
-
-    public bool this[int key] { get => Get(key); set => Set(key, value); }
 }
