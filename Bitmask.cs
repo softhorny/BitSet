14249ce4ba3c920085f42ec5777dfd2f5b84a807
bipodev <88240298+bipodev@users.noreply.github.com>
2022-01-22 12:51:00 +0300

    Update Bitmask.cs

--- Bitmask.cs
+++ Bitmask.cs
@@ -11,11 +11,19 @@ public class Bitmask
     }
 
     private byte[] _bits;
+    
     private int _length;
 
     public int Length => _length;
+    
+    public void Clear()
+    {
+        for(int i = 0; i < _bits.Length; i++)
+            _bits[i] = 0;
+    }
 
     private bool Get(int key) => (_bits[key >> 3] & (1 << key)) != 0;
+    
     private void Set(int key, bool value) 
     {
         if(value) 
