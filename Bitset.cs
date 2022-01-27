6892bb0f4335deebcf68f35a01b753f4c9200cd8
bipodev <88240298+bipodev@users.noreply.github.com>
2022-01-27 13:53:24 +0300

    Update Bitset.cs

--- Bitset.cs
+++ Bitset.cs
@@ -16,22 +16,23 @@ public class Bitset
 
     public int Length => _length;
     
-    public void Clear()
-    {
-        int length = _bits.Length;
-        for(int i = 0; i < length; i++)
-            _bits[i] = 0;
-    }
+    public bool this[int key] { get => Get(key); set => Set(key, value);
     
-    public bool this[int key] { get => Get(key); set => Set(key, value); }
-
-    private bool Get(int key) => (_bits[key >> 3] & (1 << key)) != 0;
+    public bool Get(int key) => (_bits[key >> 3] & (1 << key)) != 0;
     
-    private void Set(int key, bool value) 
+    public void Set(int key, bool value) 
     {
         if(value) 
             _bits[key >> 3] |= (byte)(1 << key);
         else 
             _bits[key >> 3] &= (byte)~(1 << key);
+    }
+    
+    public void SetAll(bool value)
+    {
+        int length = _bits.Length;
+        byte byteValue = value ? byte.MaxValue : byte.MinValue;
+        for(int i = 0; i < length; i++)
+            _bits[i] = byteValue;
     }
 }
