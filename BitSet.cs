9e015fd13edd74822dba88494d58d615326507fb
Max Linenko <88240298+softhorny@users.noreply.github.com>
2022-06-18 17:39:30 +0300

    Update BitSet.cs

--- BitSet.cs
+++ BitSet.cs
@@ -1,10 +1,9 @@
-// https://github.com/softhorny/BitSet
-
-namespace softhorny.BitSet
+namespace softh.BitSet
 {
     using System;
     using System.Runtime.CompilerServices;
     
+    // https://github.com/softhorny/BitSet
     public partial class BitSet
     {
         private const MethodImplOptions INLINE = MethodImplOptions.AggressiveInlining;
@@ -17,11 +16,11 @@ namespace softhorny.BitSet
         /// Create a new bitset of the given length. All bits are initially false. 
         /// Length will be rounded up to a multiple of 32. 
         /// </summary>
-        public BitSet(int length)
+        public BitSet( int length )
         {
-            EnsureLength(ref length);
+            EnsureLength( ref length );
 
-            _bits = length > 0 ? new uint[length] : Array.Empty<uint>();
+            _bits = length > 0 ? new uint[ length ] : Array.Empty<uint>();
         }
 
         private uint[] _bits;
@@ -31,17 +30,17 @@ namespace softhorny.BitSet
         /// </summary>
         public int Length 
         { 
-            [MethodImpl(INLINE)] 
+            [MethodImpl( INLINE )] 
             get => _bits.Length << LOG2_UINT32_MASK_SIZE; 
         }
 
-        [MethodImpl(INLINE)] 
-        private static void EnsureLength(ref int length)
+        [MethodImpl( INLINE )] 
+        private static void EnsureLength( ref int length )
         {
-            if(length < 0)
+            if( length < 0 )
                 throw new ArgumentOutOfRangeException();
 
-            length = ((length - 1) >> LOG2_UINT32_MASK_SIZE) + 1;
+            length = ( ( length - 1 ) >> LOG2_UINT32_MASK_SIZE ) + 1;
         }
     }
 
@@ -49,108 +48,108 @@ namespace softhorny.BitSet
 
     public partial class BitSet
     {
-        [MethodImpl(INLINE)] 
-        public bool Get(int key) => (_bits[key >> LOG2_UINT32_MASK_SIZE] & (1u << key)) != uint.MinValue;
+        [MethodImpl( INLINE )] 
+        public bool Get( int key ) => ( _bits[ key >> LOG2_UINT32_MASK_SIZE ] & ( 1u << key ) ) != uint.MinValue;
 
-        [MethodImpl(INLINE)] 
-        public void SetTrue(int key) => _bits[key >> LOG2_UINT32_MASK_SIZE] |= 1u << key;
+        [MethodImpl( INLINE )] 
+        public void SetTrue( int key ) => _bits[ key >> LOG2_UINT32_MASK_SIZE ] |= 1u << key;
 
-        [MethodImpl(INLINE)] 
-        public void SetFalse(int key) => _bits[key >> LOG2_UINT32_MASK_SIZE] &= ~(1u << key);
+        [MethodImpl( INLINE )] 
+        public void SetFalse( int key ) => _bits[ key >> LOG2_UINT32_MASK_SIZE ] &= ~( 1u << key );
 
-        [MethodImpl(INLINE)] 
-        public void Set(int key, bool value)
+        [MethodImpl( INLINE )] 
+        public void Set( int key, bool value )
         {
-            if(value)
-                SetTrue(key);
+            if( value )
+                SetTrue( key );
             else
-                SetFalse(key);
+                SetFalse( key );
         }
 
-        public bool this[int key] 
+        public bool this[ int key ] 
         { 
-            [MethodImpl(INLINE)] 
-            get => Get(key);
-            [MethodImpl(INLINE)] 
-            set => Set(key, value); 
+            [MethodImpl( INLINE )] 
+            get => Get( key );
+            [MethodImpl( INLINE )] 
+            set => Set( key, value ); 
         }
 
         /// <summary> 
         /// Sets the bits in the given range from (inclusive) and to (exclusive) to true. 
         /// </summary>
-        [MethodImpl(INLINE)] 
-        public void SetTrue(int from, int to)
+        [MethodImpl( INLINE )] 
+        public void SetTrue( int from, int to )
         {
-            if(from >= to)
+            if( from >= to )
                 return;
 
             int i = from >> LOG2_UINT32_MASK_SIZE, last = to - 1 >> LOG2_UINT32_MASK_SIZE;
             
             to = UINT32_MASK_SIZE - to;
 
-            if(i == last)
+            if( i == last )
             {
-                _bits[i] |= (uint.MaxValue >> to) & (uint.MaxValue << from);
+                _bits[i] |= ( uint.MaxValue >> to ) & ( uint.MaxValue << from );
                 return;
             }
 
-            _bits[i] |= uint.MaxValue << from;
-            _bits[last] |= uint.MaxValue >> to;
+            _bits[ i ] |= uint.MaxValue << from;
+            _bits[ last ] |= uint.MaxValue >> to;
 
-            for(i++; i < last; i++)
-                _bits[i] = uint.MaxValue;
+            for( i++; i < last; i++ )
+                _bits[ i ] = uint.MaxValue;
         }
 
         /// <summary> 
         /// Sets the bits in the given range from (inclusive) and to (exclusive) to false. 
         /// </summary>
-        [MethodImpl(INLINE)] 
-        public void SetFalse(int from, int to)
+        [MethodImpl( INLINE )] 
+        public void SetFalse( int from, int to )
         {
-            if(from >= to)
+            if( from >= to )
                 return;
 
             int i = from >> LOG2_UINT32_MASK_SIZE, last = to - 1 >> LOG2_UINT32_MASK_SIZE;
             
             to = UINT32_MASK_SIZE - to;
 
-            if(i == last)
+            if( i == last )
             {
-                _bits[i] &= ~((uint.MaxValue >> to) & (uint.MaxValue << from));
+                _bits[ i ] &= ~( ( uint.MaxValue >> to ) & ( uint.MaxValue << from ) );
                 return;
             }
 
-            _bits[i] &= ~(uint.MaxValue << from);
-            _bits[last] &= ~(uint.MaxValue >> to);
+            _bits[ i ] &= ~( uint.MaxValue << from );
+            _bits[ last ] &= ~( uint.MaxValue >> to );
 
-            for(i++; i < last; i++)
-                _bits[i] = uint.MinValue;
+            for( i++; i < last; i++ )
+                _bits[ i ] = uint.MinValue;
         }
 
         /// <summary> 
         /// Sets the bits in the given range from (inclusive) and to (exclusive) to the specified value. 
         /// </summary>
-        [MethodImpl(INLINE)] 
-        public void Set(int from, int to, bool value)
+        [MethodImpl( INLINE )] 
+        public void Set( int from, int to, bool value )
         {
-            if(value)
-                SetTrue(from, to);
+            if( value )
+                SetTrue( from, to );
             else
-                SetFalse(from, to);
+                SetFalse( from, to );
         }
 
         /// <summary> 
         /// Sets all bits in the bitset to the specified value. 
         /// </summary>
-        [MethodImpl(INLINE)] 
-        public void SetAll(bool value)
+        [MethodImpl( INLINE )] 
+        public void SetAll( bool value )
         {
             uint mask = value ? uint.MaxValue : uint.MinValue;
 
             uint[] bits = _bits;
 
-            for(int i = 0; i < bits.Length; i++)
-                bits[i] = mask;
+            for( int i = 0; i < bits.Length; i++ )
+                bits[ i ] = mask;
         }
     }
     #endregion
@@ -162,23 +161,23 @@ namespace softhorny.BitSet
         /// <summary> 
         /// Changes the number of bits of this bitset to the specified new length. 
         /// </summary>
-        [MethodImpl(INLINE)] 
-        public void Resize(int length)
+        [MethodImpl( INLINE )] 
+        public void Resize( int length )
         {
-            EnsureLength(ref length);
+            EnsureLength( ref length );
 
-            if(length == _bits.Length)
+            if( length == _bits.Length )
                 return;
 
-            if(length == 0)
+            if( length == 0 )
             {
                 _bits = Array.Empty<uint>();
                 return;
             }
             
-            var newArray = new uint[length];
+            var newArray = new uint[ length ];
             
-            Array.Copy(_bits, 0, newArray, 0, _bits.Length >= length ? length : _bits.Length);
+            Array.Copy( _bits, 0, newArray, 0, _bits.Length >= length ? length : _bits.Length );
 
             _bits = newArray;
         }
@@ -192,29 +191,29 @@ namespace softhorny.BitSet
         /// <summary> 
         /// Returns the population count (number of bits set) of a particular bit (0 or 1). 
         /// </summary>
-        [MethodImpl(INLINE)] 
-        public int PopCount(int key) => (int)(_bits[key >> LOG2_UINT32_MASK_SIZE] >> key & 1u);
+        [MethodImpl( INLINE )] 
+        public int PopCount( int key ) => (int)( _bits[ key >> LOG2_UINT32_MASK_SIZE ] >> key & 1u );
 
         /// <summary> 
         /// Returns the population count (number of bits set) in the given range from (inclusive) and to (exclusive). 
         /// </summary>
-        [MethodImpl(INLINE)] 
-        public int PopCount(int from, int to)
+        [MethodImpl( INLINE )] 
+        public int PopCount( int from, int to )
         {
-            if(from >= to)
+            if( from >= to )
                 return 0;
 
             int i = from >> LOG2_UINT32_MASK_SIZE, last = to - 1 >> LOG2_UINT32_MASK_SIZE;
             
             to = UINT32_MASK_SIZE - to;
 
-            if(i == last)
-                return HammingWeight(_bits[i] & ((uint.MaxValue << from) & (uint.MaxValue >> to)));
+            if( i == last )
+                return HammingWeight( _bits[ i ] & ( ( uint.MaxValue << from ) & ( uint.MaxValue >> to ) ) );
 
-            int count = HammingWeight((_bits[i] & (uint.MaxValue << from)) | ((ulong)(_bits[last] & (uint.MaxValue >> to)) << UINT32_MASK_SIZE));
+            int count = HammingWeight( ( _bits[ i ] & ( uint.MaxValue << from ) ) | ( (ulong)( _bits[ last ] & ( uint.MaxValue >> to ) ) << UINT32_MASK_SIZE ) );
 
-            for(i++; i < last; i++)
-                count += HammingWeight(_bits[i]);
+            for( i++; i < last; i++ )
+                count += HammingWeight( _bits[ i ] );
 
             return count;
         }
@@ -222,33 +221,33 @@ namespace softhorny.BitSet
         /// <summary> 
         /// Returns the population count (number of bits set) of this set. 
         /// </summary>
-        [MethodImpl(INLINE)]
+        [MethodImpl( INLINE )]
         public int PopCount()
         { 
             int count = 0;
 
-            foreach(var mask in _bits)
-                count += HammingWeight(mask);
+            foreach( var mask in _bits )
+                count += HammingWeight( mask );
 
             return count;
         }
 
-        [MethodImpl(INLINE)] 
-        private static int HammingWeight(uint mask)
+        [MethodImpl( INLINE )] 
+        private static int HammingWeight( uint mask )
         {
-            mask -= (mask >> 1) & 0x_55555555u;
-            mask = (mask & 0x_33333333u) + ((mask >> 2) & 0x_33333333u);
-            mask = (((mask + (mask >> 4)) & 0x_0F0F0F0Fu) * 0x_01010101u) >> 24;
+            mask -= ( mask >> 1 ) & 0x_55555555u;
+            mask = ( mask & 0x_33333333u ) + ( ( mask >> 2 ) & 0x_33333333u );
+            mask = ( ( ( mask + ( mask >> 4 ) ) & 0x_0F0F0F0Fu ) * 0x_01010101u ) >> 24;
 
             return (int)mask;
         }
 
-        [MethodImpl(INLINE)] 
-        private static int HammingWeight(ulong mask)
+        [MethodImpl( INLINE )] 
+        private static int HammingWeight( ulong mask )
         {
-            mask -= (mask >> 1) & 0x_55555555_55555555ul;
-            mask = (mask & 0x_33333333_33333333ul) + ((mask >> 2) & 0x_33333333_33333333ul);
-            mask = (((mask + (mask >> 4)) & 0x_0F0F0F0F_0F0F0F0Ful) * 0x_01010101_01010101ul) >> 56;
+            mask -= ( mask >> 1 ) & 0x_55555555_55555555ul;
+            mask = ( mask & 0x_33333333_33333333ul ) + ( ( mask >> 2 ) & 0x_33333333_33333333ul );
+            mask = ( ( ( mask + ( mask >> 4 ) ) & 0x_0F0F0F0F_0F0F0F0Ful ) * 0x_01010101_01010101ul ) >> 56;
 
             return (int)mask;
         }
